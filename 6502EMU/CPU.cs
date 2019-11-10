763e3af8ef636ed181e1dd60dded90706360a72d
Maurice Montag <rmontag@uw.edu>
2019-11-09 22:49:20 -0800

    Starting on implementing actual instructions.

--- 6502EMU/CPU.cs
+++ 6502EMU/CPU.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace EMU6502
 {
 
-    enum MemoryAddressingMode  // this is just the worst
+    enum MemoryAddressingMode  // this is just the worst (at least most instructions don't have all of them)
     {
         Accumulator = 0,
         Immediate = 1,
@@ -14,30 +14,32 @@ namespace EMU6502
         Zero_Page = 5,
         Indirect = 6,
         Absolute_Indexed = 7,
-        Zero_Page_Indexed = 8,
-        Indexed_Indirect = 9,
-        Indirect_Indexed = 10
+        Zero_Page_Indexed_X = 8,
+        Zero_Page_Indexed_Y = 9,
+        Indexed_Indirect = 10,
+        Indirect_Indexed = 11
     }
     class CPU
     {
-        byte[] memory;
-        ushort PC;  // program counter
-        byte status;  // status reg
+        private byte[] memory;
+        private ushort PC;  // program counter
+        private byte status;  // status reg
         /*
          *  Bit No. 7   6   5   4   3   2   1   0
                     S   V       B   D   I   Z   C
          */
-        byte SP;  // stack pointer?
-        byte x;  // register X
-        byte y;  // register Y
-        byte accum;  // accumulator register
+        private byte SP;  // stack pointer?
+        private byte X;  // register X
+        private byte Y;  // register Y
+        private byte A;  // accumulator register
+        private int cycleDelayCounter;
         readonly Stack<ushort> stack = new Stack<ushort>();  // we'll just use a stack for our stack why not?
             // On the 6502, the stack starts at 0x1FF and grows towards 0x100.
-
         // 6502 instruction operation codes (opcodes) 
         // are eight-bits long and have the general form aaabbbcc, where aaa and cc 
         // define the opcode, and bbb defines the addressing mode.
 
+        // THIS CPU IS NOT MULTITHREADED... duh
         public CPU(byte[] b)
         {
             memory = new byte[65536];  // allocate 64K of "RAM" for the CPU
@@ -47,13 +49,47 @@ namespace EMU6502
         }
         public void emulateCycle()
         {
+            if(cycleDelayCounter > 0)  // perhaps we'll get this thing to be cycle accurate :D
+            {
+                cycleDelayCounter--;
+                return;
+            }
             // opcodes are 1 byte, but the number of additional bytes is defined by the opcode itself, so we'll have to increment the program counter by a variable number
             byte opcode = memory[PC];  // get the opcode
 
             switch (opcode)  // there's got to be a better way to organize this. 
             {
-              //  case 0x69:  // 
+               case 0xA2:  // LDX (Load index X with memory)  immediate
+                    LDX(MemoryAddressingMode.Immediate);
+                    break;
+                    
                         
+            }
+        }
+
+        private void LDX(MemoryAddressingMode addressingMode)
+        {
+            ushort memLocation;
+            switch (addressingMode)
+            {
+                case MemoryAddressingMode.Immediate:  // cool switching on enum
+                    // okay for this instruction we load the next byte into the X register
+                    X = memory[PC + 1];  // load the next byte into the X register
+                    PC += 2;  // increment program counter by 2 as specified.
+                    cycleDelayCounter = 2;  // this command takes 2 cycles
+                    break;
+                case MemoryAddressingMode.Zero_Page:
+                    memLocation = memory[PC + 1];  // hopefully we zero extend out to 16 bits like we should
+                    X = memory[memLocation];  // load the data at that zero page memory location into X
+                    PC += 2;  // increment program counter as specified
+                    cycleDelayCounter = 3;  // this command takes 3 cycles
+                    break;
+                case MemoryAddressingMode.Zero_Page_Indexed_Y:
+                    // The value in Y is added to the specified zero page address for a sum address. The value at the sum address is used to perform the computation.
+                    memLocation = memory[PC + 1];  // hopefully we zero extend out to 16 bits like we should
+                    X = memory[memLocation+Y];
+                    PC += 2;  // increment program counter as specified
+                    break;
             }
         }
 
@@ -61,6 +97,17 @@ namespace EMU6502
         {
 
         }
+
+
+
+
+
+
+
+
+
+
+        // HELPER METHODS FOR SETTING THE VARIOUS PROCESSOR FLAGS
 
         private void SetCarryFlag(bool b) 
                 // his holds the carry out of the most significant bit in any arithmetic operation. 
