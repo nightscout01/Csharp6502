d14723bf1cccbe614f235a8b306de8da81747c5b
Maurice Montag <rmontag@uw.edu>
2019-12-26 00:47:03 -0800

    cleaned up comment

--- 6502EMU/ConsoleMain.cs
+++ 6502EMU/ConsoleMain.cs
@@ -11,7 +11,7 @@ namespace EMU6502
     {
         private const string FILE_LOCATION = @"C:\Users\night\Downloads\6502_functional_test.bin";  // replace with path to your ROM
 
-        private const ushort START_LOCATION = 0x400;  // this isn't as rigid as the usual CHIP8 programs, we should really be using the 6502
+        private const ushort START_LOCATION = 0x400;  // this isn't as rigid as CHIP8, we should really be using the 6502
             // reset vector to determine program start location, using the CPU constructor without the start location argument will use this vector
             // as contained in the ROM.
         public static void Main(string[] args)
